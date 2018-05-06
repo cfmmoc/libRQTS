310ba24bff4c7619ebe0c9282b65ded9cb868b84
cfMMOC for terrain rendering library <35916245+cfmmoc@users.noreply.github.com>
2018-05-06 11:15:01 +0800

    add comment

--- neig.cpp
+++ neig.cpp
@@ -33,12 +33,26 @@
        .@#       .@@@@. @   ,,   @@@@@@@@@@@@#        .@@@@@@@@    .,        @@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@**/
 
+/**
+	@brief  A class representing neighbors of a tile.
+**/
+
 #include "neig.h"
 
+/**
+    	@remarks
+        	defualt constructor
+**/
 CTBNeighbor::CTBNeighbor(void)
 {
 }
 
+/**
+    	@remarks
+        	initialize eight neighbors (two neighbors on each side of a square) of a tile
+    	@par
+        	*neighbor	filenames of eight neighbors
+**/
 CTBNeighbor::CTBNeighbor(std::string* neighbor)
 {
 	for (int i = 0; i < 8; i++)
@@ -47,15 +61,32 @@ CTBNeighbor::CTBNeighbor(std::string* neighbor)
 	}
 }
 
+/**
+    	@remarks
+        	defualt deconstructor
+**/
 CTBNeighbor::~CTBNeighbor(void)
 {
 }
 
+/**
+    	@remarks
+        	return neighbor of a tile for given side
+    	@par
+        	i	given side (from 0 to 7)
+**/
 std::string CTBNeighbor::GetNeighbor(int i)
 {
 	return mNeighbor[i];
 }
 
+/**
+    	@remarks
+        	update the filename of neighbor of a tile for given side
+    	@par
+        	i	given side (from 0 to 7)
+		name	filename of neighbor
+**/
 void CTBNeighbor::SetNeighbor(int i, std::string name)
 {
 	mNeighbor[i] = name;
