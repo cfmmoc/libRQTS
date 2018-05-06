79b6842f1012b305ad164aff679b766a17f5f82a
cfMMOC for terrain rendering library <35916245+cfmmoc@users.noreply.github.com>
2018-05-06 11:07:14 +0800

    add comment

--- lreq.cpp
+++ lreq.cpp
@@ -33,12 +33,27 @@
        .@#       .@@@@. @   ,,   @@@@@@@@@@@@#        .@@@@@@@@    .,        @@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@**/
 
+/**
+  @brief  A class representing a load request.
+**/
+
 #include "lreq.h"
 
+/**
+    	@remarks
+        	default constructor
+**/
 CTBLoadReq::CTBLoadReq(void)
 {
 }
 
+/**
+    	@remarks
+        	initialize a load request
+    	@par
+        	split	indicates request type (split/merge)
+		name	filename of the request
+**/
 CTBLoadReq::CTBLoadReq(bool split, std::string name)
 {
 	mSplitReq = split;
@@ -47,30 +62,56 @@ CTBLoadReq::CTBLoadReq(bool split, std::string name)
 	mReqOverCount = 0;
 }
 
+/**
+    	@remarks
+        	default deconstructor
+**/
 CTBLoadReq::~CTBLoadReq(void)
 {
 }
 
+/**
+    	@remarks
+        	return type of load request, true for split request
+**/
 bool CTBLoadReq::IsSplitReq()
 {
 	return mSplitReq;
 }
 
+/**
+    	@remarks
+        	return filename of load request
+**/
 std::string CTBLoadReq::GetReqName()
 {
 	return mReqName;
 }
 
+/**
+    	@remarks
+        	return counter of request
+**/
 int CTBLoadReq::GetReqCount()
 {
 	return mReqCount;
 }
 
+/**
+    	@remarks
+        	return counter of finished request
+**/
 int CTBLoadReq::GetReqOverCount()
 {
 	return mReqOverCount;
 }
 
+/**
+    	@remarks
+        	increase counter of request by one
+		for split request, the maximum value of counter is four
+		for merge request, the maximum value of counter is one
+**/
 void CTBLoadReq::IncReqCount()
 {
 	if (mSplitReq && mReqCount < 4)
@@ -83,6 +124,11 @@ void CTBLoadReq::IncReqCount()
 	}
 }
 
+/**
+    	@remarks
+        	increase counter of finished request by one
+		for split request, the maximum value of counter is four
+**/
 void CTBLoadReq::IncReqOverCount()
 {
 	if (mSplitReq && mReqOverCount < 4)
