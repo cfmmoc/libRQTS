2158802d4e310c198b4cb981b94f2ca5d7e5eec1
cfMMOC for terrain rendering library <35916245+cfmmoc@users.noreply.github.com>
2018-05-07 17:14:36 +0800

    add comment

--- rqts.h
+++ rqts.h
@@ -41,23 +41,84 @@
 #include "neig.h"
 #include "lreq.h"
 
+/**
+    	@remarks
+        	replace the trailing letter of filename
+**/
+
 std::string replaceNamend(std::string name);
+
+/**
+	@brief  A class representing neighbors of a tile.
+**/
 
 class libRQTS
 {
 public:
+	/**
+    	@remarks
+        	allocate shared memory and initialize neighbors of initial tiles
+	**/
 	libRQTS(void);
+	/**
+    	@remarks
+        	free shared memory
+	**/
 	~libRQTS(void);
+	/**
+    	@remarks
+        	check whether a tile could be split or not
+	**/
 	bool checksplit(std::string filename);
+	/**
+    	@remarks
+        	check whether a list of tiles could be merged or not
+	**/
 	bool checkmerge(std::vector<std::string> filenames);
+	/**
+    	@remarks
+        	check whether a tile could be merged or not
+	**/
 	bool checkMerge(std::string filename);
+	/**
+    	@remarks
+        	actual operation of merging a tile
+	**/
 	void merge(std::string filename);
+	/**
+    	@remarks
+        	actual operation of splitting a tile
+	**/
 	void split(std::string filename);
+	/**
+    	@remarks
+        	check load queue, and return load request from load queue
+	**/
 	std::string checkfile();
+	/**
+    	@remarks
+        	return load request from load queue
+	**/
 	std::string getfile();
+	/**
+    	@remarks
+        	return whether load queue is empty, true if empty
+	**/
 	bool isdone();
+	/**
+    	@remarks
+        	send load requests to fore-end process
+	**/
 	void sendfile();
+	/**
+    	@remarks
+        	receive load requests from back-end process
+	**/
 	bool recvfile();
+	/**
+    	@remarks
+        	check neighbors of given tile
+	**/
 	std::string checkbound(std::string filename, unsigned char side, unsigned char* mask);
 
 private:
